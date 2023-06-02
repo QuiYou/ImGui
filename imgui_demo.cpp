cb25afd5c04574e9c5d5d96c0f3384345b41e047
ocornut <omarcornut@gmail.com>
2023-06-02 15:49:17 +0200

    RangeSelect/MultiSelect: (Breaking) Renamed 'RangeSrc -> 'RangeSrcItem', "RangeDst' -> 'RangeDstItem'
    This is necessary to have consistent names in upcoming fields (NavIdItem etc.)

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -2741,7 +2741,7 @@ struct ExampleSelection
     {
         if (ms_io->RequestClear)        { Clear(); }
         if (ms_io->RequestSelectAll)    { SelectAll(items_count); }
-        if (ms_io->RequestSetRange)     { SetRange((int)(intptr_t)ms_io->RangeSrc, (int)(intptr_t)ms_io->RangeDst, ms_io->RangeValue ? 1 : 0); }
+        if (ms_io->RequestSetRange)     { SetRange((int)(intptr_t)ms_io->RangeSrcItem, (int)(intptr_t)ms_io->RangeDstItem, ms_io->RangeValue ? 1 : 0); }
     }
 };
 
@@ -2830,7 +2830,7 @@ static void ShowDemoWindowMultiSelect()
 
                 // Apply multi-select requests
                 ms_io = ImGui::EndMultiSelect();
-                selection.RangeRef = (int)(intptr_t)ms_io->RangeSrc;
+                selection.RangeRef = (int)(intptr_t)ms_io->RangeSrcItem;
                 selection.ApplyRequests(ms_io, ITEMS_COUNT);
 
                 ImGui::EndListBox();
@@ -2871,7 +2871,7 @@ static void ShowDemoWindowMultiSelect()
 
                 // Apply multi-select requests
                 ms_io = ImGui::EndMultiSelect();
-                selection->RangeRef = (int)(intptr_t)ms_io->RangeSrc;
+                selection->RangeRef = (int)(intptr_t)ms_io->RangeSrcItem;
                 selection->ApplyRequests(ms_io, ITEMS_COUNT);
                 ImGui::PopID();
             }
@@ -2938,9 +2938,9 @@ static void ShowDemoWindowMultiSelect()
 
                 while (!use_clipper || clipper.Step())
                 {
-                    // IF clipping is used you need to set 'RangeSrcPassedBy = true' if RangeSrc was passed over.
+                    // IF clipping is used you need to set 'RangeSrcPassedBy = true' if RangeSrcItem was passed over.
                     if (use_clipper)
-                        if ((int)(intptr_t)ms_io->RangeSrc <= clipper.DisplayStart)
+                        if ((int)(intptr_t)ms_io->RangeSrcItem <= clipper.DisplayStart)
                             ms_io->RangeSrcPassedBy = true;
 
                     const int item_begin = use_clipper ? clipper.DisplayStart : 0;
@@ -3028,7 +3028,7 @@ static void ShowDemoWindowMultiSelect()
 
                 // Apply multi-select requests
                 ms_io = ImGui::EndMultiSelect();
-                selection.RangeRef = (int)(intptr_t)ms_io->RangeSrc;
+                selection.RangeRef = (int)(intptr_t)ms_io->RangeSrcItem;
                 selection.ApplyRequests(ms_io, ITEMS_COUNT);
 
                 if (widget_type == WidgetType_TreeNode)
