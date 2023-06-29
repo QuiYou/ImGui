940ce3e539ce4f297aedfb2a227b63acfcf27c76
ocornut <omarcornut@gmail.com>
2023-06-29 17:00:43 +0200

    RangeSelect/MultiSelect: Remove the need for using IsItemToggledSelection(). Update comments.
    This is the simple version that past our tests. MultiSelectItemFooter() is in need of a cleanup.

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -2825,8 +2825,6 @@ static void ShowDemoWindowMultiSelect()
                     bool item_is_selected = selection.GetSelected(n);
                     ImGui::SetNextItemSelectionUserData(n);
                     ImGui::Selectable(label, item_is_selected);
-                    if (ImGui::IsItemToggledSelection())
-                        selection.SetSelected(n, !item_is_selected);
                 }
 
                 // Apply multi-select requests
@@ -2866,8 +2864,6 @@ static void ShowDemoWindowMultiSelect()
                     bool item_is_selected = selection->GetSelected(n);
                     ImGui::SetNextItemSelectionUserData(n);
                     ImGui::Selectable(label, item_is_selected);
-                    if (ImGui::IsItemToggledSelection())
-                        selection->SetSelected(n, !item_is_selected);
                 }
 
                 // Apply multi-select requests
@@ -2969,8 +2965,6 @@ static void ShowDemoWindowMultiSelect()
                         if (widget_type == WidgetType_Selectable)
                         {
                             ImGui::Selectable(label, item_is_selected);
-                            if (ImGui::IsItemToggledSelection())
-                                selection.SetSelected(n, !item_is_selected);
                             if (use_drag_drop && ImGui::BeginDragDropSource())
                             {
                                 ImGui::Text("(Dragging %d items)", selection.GetSize());
@@ -2984,8 +2978,6 @@ static void ShowDemoWindowMultiSelect()
                             if (item_is_selected)
                                 tree_node_flags |= ImGuiTreeNodeFlags_Selected;
                             bool open = ImGui::TreeNodeEx(label, tree_node_flags);
-                            if (ImGui::IsItemToggledSelection())
-                                selection.SetSelected(n, !item_is_selected);
                             if (use_drag_drop && ImGui::BeginDragDropSource())
                             {
                                 ImGui::Text("(Dragging %d items)", selection.GetSize());
