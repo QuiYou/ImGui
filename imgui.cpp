5e03a9100c8023345c02d03e88df1567b7d330d8
ocornut <omarcornut@gmail.com>
2023-09-22 14:30:56 +0200

    RangeSelect/MultiSelect: added missing call on Shutdown(). Better reuse selection buffer.

--- imgui.cpp
+++ imgui.cpp
@@ -3669,6 +3669,8 @@ void ImGui::Shutdown()
     g.TablesTempData.clear_destruct();
     g.DrawChannelsTempMergeBuffer.clear();
 
+    g.MultiSelectStorage.Clear();
+
     g.ClipboardHandlerData.clear();
     g.MenusIdSubmittedThisFrame.clear();
     g.InputTextState.ClearFreeMemory();
